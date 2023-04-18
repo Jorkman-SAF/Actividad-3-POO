75942ece5b81f18ffebecfc9957b58f6bbcef99b
Jorkman-SAF <124703679+Jorkman-SAF@users.noreply.github.com>
2023-04-17 22:51:20 -0500

    Update Ejercicio_19.py

--- Parte 1/Ejercicio 19/Ejercicio_19.py
+++ Parte 1/Ejercicio 19/Ejercicio_19.py
@@ -1,28 +1,66 @@
 import tkinter as tk
+from tkinter import messagebox
+import math
 
-def calcular():
-    lado = float(lado_entry.get())
+class TrianguloEquilatero:
+    def __init__(self, lado):
+        self.lado = lado
+        
+    def calcular_perimetro(self):
+        return 3 * self.lado
+    
+    def calcular_altura(self):
+        return (math.sqrt(3) / 2) * self.lado
+    
+    def calcular_area(self):
+        return (math.sqrt(3) / 4) * self.lado ** 2
 
-    perimetro = 3 * lado
-    altura = (lado * (3 ** 0.5)) / 2
-    area = (lado ** 2) * (3 ** 0.5) / 4
-
-    resultado_text.set("El perímetro del triángulo es: {}\nLa altura del triángulo es: {}\nEl área del triángulo es: {}".format(perimetro, altura, area))
-
-ventana = tk.Tk()
-ventana.title("Cálculo de Triángulo Equilátero")
-
-lado_label = tk.Label(ventana, text="Ingrese el valor del lado del triángulo equilátero:")
-lado_label.pack()
-
-lado_entry = tk.Entry(ventana)
-lado_entry.pack()
-
-calcular_button = tk.Button(ventana, text="Calcular", command=calcular)
-calcular_button.pack()
-
-resultado_text = tk.StringVar()
-resultado_label = tk.Label(ventana, textvariable=resultado_text)
-resultado_label.pack()
-
-ventana.mainloop()
+class VentanaPrincipal:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Triángulo Equilátero")
+        
+        self.lbl_lado = tk.Label(self.root, text="Valor del lado:")
+        self.lbl_lado.pack()
+        self.entry_lado = tk.Entry(self.root)
+        self.entry_lado.pack()
+        
+        self.btn_calcular = tk.Button(self.root, text="Calcular", command=self.calcular_resultados)
+        self.btn_calcular.pack()
+        
+        self.lbl_perimetro = tk.Label(self.root, text="")
+        self.lbl_perimetro.pack()
+        
+        self.lbl_altura = tk.Label(self.root, text="")
+        self.lbl_altura.pack()
+        
+        self.lbl_area = tk.Label(self.root, text="")
+        self.lbl_area.pack()
+        
+    def calcular_resultados(self):
+        lado = self.entry_lado.get()
+        
+        if lado == "":
+            messagebox.showerror("Error", "Por favor ingrese un valor para el lado.")
+            return
+        
+        try:
+            lado = float(lado)
+        except ValueError:
+            messagebox.showerror("Error", "El valor del lado debe ser un número.")
+            return
+        
+        triangulo = TrianguloEquilatero(lado)
+        
+        perimetro = triangulo.calcular_perimetro()
+        self.lbl_perimetro.configure(text=f"El perímetro es {perimetro:.2f}.")
+        
+        altura = triangulo.calcular_altura()
+        self.lbl_altura.configure(text=f"La altura es {altura:.2f}.")
+        
+        area = triangulo.calcular_area()
+        self.lbl_area.configure(text=f"El área es {area:.2f}.")
+        
+root = tk.Tk()
+ventana_principal = VentanaPrincipal(root)
+root.mainloop()
